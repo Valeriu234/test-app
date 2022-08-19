3f401b2a218a57b675d42dce2f699c623fc749a1
Valeriu234 <cazacuvaleriu2@gmail.com>
2022-08-19 18:58:08 +0300

    Am mai adaugat ceva

---
+++ src/Components/Right-bar/Right-bar-rca.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const RightBarRca = () => {
+    return (
+        <div className='right-bar'>
+            <div className="first-step round">
+                <div className="punct">
+                </div>
+            </div>
+            <hr className='relation-line rca'/>
+            <div className="second-step round">
+                <div className="count">
+                    1
+                </div>
+            </div>
+            <hr className='relation-line rca'/>
+            <div className="third-step round">
+                <div className="count">
+                    2
+                </div>
+            </div>
+            <hr className='relation-line  rca'/>
+            <div className="fourth-step round">
+                <div className="count">
+                    3
+                </div>
+            </div>
+            <hr className='relation-line rca'/>
+            <div className="fourth-step round">
+                <div className="count">
+                    4
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RightBarRca;
